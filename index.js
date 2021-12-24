f1d48babb2a382b57be9076a594374a4a93e3755
Charles <c_onyemem@live.com>
2021-12-23 22:58:40 -0800

    commit tree 8d385b8cf194630ca23b50a87a9f0f7ad51bab2c

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+
+   const postingObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify({
+        name: `${name}`,
+        email: `${email}`
+    })
+
+    }
+     return fetch('http://localhost:3000/users', postingObj)
+        .then(resp => resp.json())
+        .then(obj => {
+            const newId = obj.id
+            document.querySelector('body').append(newId)
+        })
+    .catch(function(error){
+        const errorMessage = error.message
+        document.querySelector('body').append(errorMessage)
+    })
+    
+}
+
+submitData()
